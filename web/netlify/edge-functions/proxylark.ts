606252c4f3d97edd38a7f133ae2c7abfed3d7bad
xna <xna00@qq.com>
2023-02-22 20:39:20 +0800

    auth

--- web/netlify/edge-functions/proxylark.ts
+++ web/netlify/edge-functions/proxylark.ts
@@ -1,7 +1,21 @@
+import { Context } from "https://edge.netlify.com";
 import { fetchLark } from "../lark.ts";
 
-export default (req: Request) => {
+const KEY = 'SECRET_TOKEN'
+
+export default (req: Request, context: Context) => {
     const url = req.url.replace(/^.+?proxylark\//, "");
+    console.log(context.cookies.get(KEY), Deno.env.get(KEY))
+    if (!context.cookies.get(KEY)) {
+        return new Response(null, {
+            status: 401
+        })
+    }
+    if (context.cookies.get(KEY) !== Deno.env.get(KEY)) {
+        return new Response(null, {
+            status: 403
+        })
+    }
     return fetchLark(url, {
         headers: req.headers,
         method: req.method,
